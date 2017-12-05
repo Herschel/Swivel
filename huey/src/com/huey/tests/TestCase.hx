019ebcd83f15fc10b1795590bb4cc7f9a8cf0655
Mike Welsh <mwelsh@gmail.com>
2017-12-04 20:07:21 -0800

    chore: add license and copyrights to all source files

--- huey/src/com/huey/tests/TestCase.hx
+++ huey/src/com/huey/tests/TestCase.hx
@@ -1,3 +1,22 @@
+/*
+ * Swivel
+ * Copyright (C) 2012-2017, Newgrounds.com, Inc.
+ * https://github.com/Herschel/Swivel
+ *
+ * Swivel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Swivel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Swivel.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.huey.tests;
 import haxe.Stack;
 
