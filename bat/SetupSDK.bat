ecb4ba267cc74e391c89d37186aef1ae7fa52acc
Mike Welsh <mwelsh@gmail.com>
2017-12-07 12:43:27 -0800

    chore: look for AIR_SDK environemnt var
    SetupSDK.bat will now look for the AIR_SDK environment var to
    specify the path to the Adobe AIR SDK. If this var is unset, the
    path will be set to location that HaxeDevelop installs to by
    default. The AIR SDK can be installed from HaxeDevelop by going to
    Tools->Install Software and checking AIR SDK + ASC 2.0.
    
    This will pave the way to automated packaging via AppVeyor and
    Travis.

--- bat/SetupSDK.bat
+++ bat/SetupSDK.bat
@@ -1,21 +1,21 @@
 :user_configuration
 
-:: Path to Flex SDK
-set FLEX_SDK=%HOMEDRIVE%%HOMEPATH%\AppData\Local\HaxeDevelop\Apps\ascsdk\27.0.0
+:: Default path to AIR SDK if installed using HaxeDevelop.
+if not defined AIR_SDK (set AIR_SDK=%HOMEDRIVE%%HOMEPATH%\AppData\Local\HaxeDevelop\Apps\ascsdk\27.0.0)
 
 :validation
-if not exist "%FLEX_SDK%\bin" goto flexsdk
+if not exist "%AIR_SDK%\bin" goto flexsdk
 goto succeed
 
 :flexsdk
 echo.
-echo ERROR: incorrect path to Flex SDK in 'bat\SetupSDK.bat'
-echo.
-echo Looking for: %FLEX_SDK%\bin
+echo ERROR: Path to Air SDK not set.
+echo Please set the AIR_SDK environment variable, or install the 
+echo AIR SDK + ASC 2.0 Compiler in HaxeDevelop -> Tools -> Install Software.
 echo.
 if %PAUSE_ERRORS%==1 pause
 exit
 
 :succeed
-set PATH=%PATH%;%FLEX_SDK%\bin
+set PATH=%PATH%;%AIR_SDK%\bin
 
